91059d0ff0fc48f585f8036d6ef5af01fd00ff16
Hala Elsiddig <h3l3.siddi8@gmail.com>
2020-11-10 13:18:25 -0500

    third commit

--- Homework.cpp
+++ Homework.cpp
@@ -44,7 +44,41 @@ for(int z=0;z<5;z=z+1){
     else
         cout << "Error";
 
+    int p=1;
+    do{
+      cout << "enter name: ";
+    string name;
+    cin >> name;
 
-}
+
+    cout << "enter the grade:";
+    cin >> grade;
+    if ((grade >= 90) && (grade) <= 100)
+        cout << "A\n";
+    else if ((grade < 90) && (grade) >= 80)
+        cout << "B\n";
+    else if ((grade < 80) && (grade) >= 70)
+        cout << "C\n";
+    else if ((grade < 70) && (grade) >= 60)
+        cout << "D\n";
+    else if ((grade < 60) && (grade) >= 50)
+        cout << "F\n";
+
+
+    cout << "enter the Department: ";
+    cin >> y;
+    if (y == 'A')
+        cout << "Control Department" << endl;
+    else if (y == 'T')
+        cout << "Communication Department" << endl;
+    else
+        cout << "Error";
+
+        p=p++;
+
+
+
+
+}while(p!="exit");
     return 0;
 }
